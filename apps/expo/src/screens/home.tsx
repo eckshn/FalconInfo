31825d34e6b88e1b85fe313998e929b405bfd8db
student <eckshn@gmail.com>
2023-03-09 14:32:16 -0500

    Set up background

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -3,3 +3,7 @@ import { Button, Modal, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { api } from "../utils/api";
+
+export const HomeScreen = () => {
+  return <SafeAreaView className="bg-[#262626]"></SafeAreaView>;
+};
