60cd1312682a0d1e459491d4e339c87a34209f57
Thomas Zhong <zhongt900@gmail.com>
2023-03-10 13:58:01 -0500

    tailwind theme
    added our custom color palette

--- packages/config/tailwind/index.js
+++ packages/config/tailwind/index.js
@@ -2,6 +2,14 @@
 const config = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      darkgrey: "#262626",
+      gold: "EFAE04",
+      white: "F5F5F5",
+      grey: "373737",
+      green: "85C96D",
+      blue: "6DC9C3",
+    },
     extend: {},
   },
   plugins: [],
